3a69752e7f7174c5030f40d8ce27b26ac2521c34
Edman Feng <edmanfeng@hotmail.com>
2017-03-30 05:56:13 -0700

    18.9: done

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -1,5 +1,8 @@
 package com.bignerdranch.android.beatbox;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+
 /**
  * Created by t7500 on 3/30/2017.
  */
@@ -10,4 +13,11 @@ public class BeatBox {
 
     /** Folder that sounds are saved in */
     private static final String SOUNDS_FOLDER = "sample_sounds";
+
+    /** The asset manager used by this BeatBox to access assets */
+    private AssetManager mAssets;
+
+    public BeatBox(Context context) {
+        mAssets = context.getAssets();
+    }
 }
