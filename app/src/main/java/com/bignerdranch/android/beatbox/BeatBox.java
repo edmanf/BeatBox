348ff809847c8b029919d734d62d3d884adf423a
Edman Feng <edmanfeng@hotmail.com>
2017-03-30 05:53:29 -0700

    18.8: done

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.beatbox;
+
+/**
+ * Created by t7500 on 3/30/2017.
+ */
+
+public class BeatBox {
+    /** Constant for logging */
+    private static final String TAG = "BeatBox";
+
+    /** Folder that sounds are saved in */
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+}
