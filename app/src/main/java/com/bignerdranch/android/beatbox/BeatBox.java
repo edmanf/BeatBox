67b1b84fa799d7b554133556d1af66c288e76535
Edman Feng <edmanfeng@hotmail.com>
2017-03-30 05:59:12 -0700

    18.10: done

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -2,6 +2,9 @@ package com.bignerdranch.android.beatbox;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
 
 /**
  * Created by t7500 on 3/30/2017.
@@ -18,6 +21,21 @@ public class BeatBox {
     private AssetManager mAssets;
 
     public BeatBox(Context context) {
+        // Usually don't need to worry about what context is passed in
+        // they usually point to same set of assets
         mAssets = context.getAssets();
+        loadSounds();
     }
+
+    private void loadSounds() {
+        String[] soundNames;
+        try {
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        } catch (IOException ioe) {
+            Log.e(TAG, "Could not list assets", ioe);
+            return;
+        }
+    }
+
 }
